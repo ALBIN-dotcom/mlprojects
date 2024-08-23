c4aecd3398240021717d1836f33f0d30795cd0b5
ALBIN-dotcom <albinjoseph1810@gmail.com>
2024-08-23 10:27:54 +0530

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetiime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
@@ -14,6 +14,6 @@ logging.basicConfig(
     level=logging.INFO,
     
 )
+if __name__=="__main__":
+    logging.info("logging has started")
 
-if__name__=="__main__":
-    logging.INFO("Logging has started")
